06584155aa3b30d5278ef339735c854280ec878d
Augustin de Maintenant <augustin.demaintenant@gmail.com>
2020-01-21 14:56:43 +0100

    plugged webservice to get weather data

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var session = require("express-session");
+
+
+app.use(
+  session({ 
+  secret: 'a4f8071f-c873-4447-8ee2',
+  resave: false,
+  saveUninitialized: false,
+  })
+  );
+  
 var app = express();
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('sync-request');
 
 /* météo data */
 
@@ -18,6 +19,15 @@ router.get('/login', function(req, res, next) {
 /* Add city */
 router.post('/add-city', function(req, res, next) {
 
+
+  /* Appel de l'API pour receuillir les informations météo de la ville saisie */
+  try {
+  var resp = request("GET", `https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&units=metric&lang=fr&APPID=a512ea6e86a6184fab6c416f00248b27`);
+
+  var respJSON = JSON.parse(resp.getBody());
+
+  /* Push des informations météo de la ville dans le tableau (si la ville n'a pas déjà été entrée et si la requête n'est pas vide) */
+
   var findCity = cityList.findIndex(element => element.ville.toLowerCase() === req.body.city.toLowerCase());
   
   console.log("index de la ville:", findCity);
@@ -26,14 +36,18 @@ router.post('/add-city', function(req, res, next) {
   if(findCity === -1 && req.body.city !== "") { 
     cityList.push(
     {
-      ville: req.body.city,
-      temps : "nuageux",
-      picto : "/images/picto-1.png",
-      temp_max : 10.2,
-      temp_min : 7.5,
+      ville: respJSON.name,
+      temps : respJSON.weather[0].description,
+      picto : `http://openweathermap.org/img/wn/${respJSON.weather[0].icon}@2x.png`,
+      temp_max : respJSON.main.temp_max,
+      temp_min : respJSON.main.temp_min,
       delete : "/images/delete.png"
     }
   )
+  }
+
+  } catch (err) {
+    console.log(err)
   }
 
   res.render('index', { cityList });
